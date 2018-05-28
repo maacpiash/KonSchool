3c3e72cd60586192d56c61af880e5ba2b5f0cc29
Piash at STML <maacpiash@outlook.com>
2018-05-28 18:38:00 +0600

    Worked on CSVreader class.

--- KonSchool_Models/CSVreader.cs
+++ KonSchool_Models/CSVreader.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Globalization;
+using Microsoft.AspNetCore.JsonPatch.Helpers;
+using static System.Array;
+
 namespace KonSchool_Models
 {
     public class CSVreader
@@ -31,32 +36,33 @@ namespace KonSchool_Models
         // DIVISION DISTRICT THANA UNION MAUZA AREA_STATUS GEOGRPYCAL_STATUS ADDRESS POST MOBILE
         // MANAGEMENT_TYPE STUDENT_TYPE LAVER_OF_EDUCATION EDUCATION_LEVEL
         // AFFILIATION	MPO_STATUS	SCHOOL_AGE	JUNIOR_MPO_AGE	SECONDARY_MPO_AGE
+
+        private string[][] dataLines;
+        private string[] attributes;
+        private int[] eiins;
+        private int keyIndex;
+        private int height, width;
+
+        public (int, int) Dimenstion => (height, width);
+
+        public string[] Attributes => attributes;
         
-        string[][] dataLines;
-        string[] keys;
+        public string this[int EIIN, string Attribute] => dataLines[IndexOf(eiins, EIIN)][IndexOf(attributes, Attribute)];
 
-        public string[] Keys { get => keys; set => keys = value; }
-
-        public int this[string key]
-        {
-            get => GetValue(key);
-            set => SetValue(key, value);
-        }
-
-        private int GetValue(string key)
-        {
-            return 0;
-        }
-
-        private void SetValue(string key, int value)
-        {
-
-        }
-
-        public CSVreader(string[] lines)
-        {
-            int height, width;
-
+        public CSVreader(string[] Lines)
+        { 
+            height = Lines.Length - 1;
+            attributes = Lines[0].Split(',');
+            width = attributes.Length;
+            keyIndex = IndexOf(attributes, "EIIN");
+            eiins = new int[height];
+            dataLines = new string[height][];
+            
+            for (int i = 1; i <= height; i++)
+            {
+                dataLines[i - 1] = Lines[i].Split(',');
+                eiins[i - 1] = Convert.ToInt32(dataLines[i - 1][keyIndex]);
+            }
         }
 
     }    
