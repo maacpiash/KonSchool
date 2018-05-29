48dabf08fb93c067b274c297c525c6b2b0a02876
Piash at STML <maacpiash@outlook.com>
2018-05-29 18:18:43 +0600

    Added test project. Models passed the tests.

---
+++ KonSchool_Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using KonSchool_Models;
+
+using static System.Console;
+using static System.IO.File;
+
+namespace KonSchool_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSVreader myreader = new CSVreader(ReadAllLines("dataset.csv"));
+            string[] criteria = myreader.Attributes;
+            for(int i = 0; i < 5; i++)
+                WriteLine(criteria[i]);
+                // supposed to write the list of criteria
+            WriteLine(myreader[102, "SCHOOL NAME"]);
+            // supposed to write "IDEAL SCHOOL AND COLLEGE"
+        }
+    }
+}
