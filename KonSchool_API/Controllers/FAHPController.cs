e985378e77e54aebf4644d4e224aa5f751d7b699
piash-at-stml <maacpiash@outlook.com>
2018-08-06 19:59:57 +0600

    API now works asynchronously. Added CI for macOS.

--- KonSchool_API/Controllers/FAHPController.cs
+++ KonSchool_API/Controllers/FAHPController.cs
@@ -22,49 +22,59 @@ namespace KonSchool_API.Controllers
 
         // GET api/fahp/6,5.0,1,13,RAJSHAHI,SIRAJGANJ,KAMARKHANDA,ROY DAULATPUR,-1,2,1,-3,1,3,1,0,3,0,-2,0,-2,0,3,...
         [HttpGet("{variables}")]
-        public ActionResult<string> Get(string variables)
+        public async Task<ActionResult> Get(string variables)
         {
             if (!variables.Contains(','))
-                return "Where are the other values?!";
+                return Ok("Where are the other values?!");
             string[] values = variables.Split(',');
             int max = values.Length;
             if (max < 22)
-                return "Where are the other values?!";
-            string fp = Directory.GetCurrentDirectory();
+                return Ok("Where are the other values?!");
+            string retVals = await GetValuesAsync(values);
+            return Ok(retVals);
+        }
 
-            fp = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, "Dataset.csv");
-            Query query;
-            try
+        private Task<string> GetValuesAsync(string[] values)
+        {
+            return Task.Run(() =>
             {
-                query = new Query(6, fp)
+                int max = values.Length;
+                string fp = Directory.GetCurrentDirectory();
+
+                fp = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, "Dataset.csv");
+                Query query;
+                try
                 {
-                    Class = Convert.ToInt32(values[0]),
-                    Social = Convert.ToDouble(values[1]),
-                    IsMale = values[2] == "1",
-                    Age = Convert.ToInt32(values[3]),
-                    Location = new Address(values[4], values[5], values[6], values[7])
-                };
-                for (int i = 0; i < 15; i++)
-                    query.FuzzyValues[i] = Convert.ToInt32(values[i + 8]);
-            }
-            catch (Exception x)
-            {
-                return x.ToString();
-            }
+                    query = new Query(6, fp)
+                    {
+                        Class = Convert.ToInt32(values[0]),
+                        Social = Convert.ToDouble(values[1]),
+                        IsMale = values[2] == "1",
+                        Age = Convert.ToInt32(values[3]),
+                        Location = new Address(values[4], values[5], values[6], values[7])
+                    };
+                    for (int i = 0; i < 15; i++)
+                        query.FuzzyValues[i] = Convert.ToInt32(values[i + 8]);
+                }
+                catch (Exception x)
+                {
+                    return x.ToString();
+                }
 
 
-            query.CreateComparisonMatrix();
-            FAHP fAHP = new FAHP(query.ComparisonMatrix);
-            double[] weights = fAHP.CriteriaWeights;
-            if (max == 23)
-                return string.Join(',', weights);
-            int[] choices = new int[max - 23];
-            for (int i = 23; i < max; i++)
-                choices[i - max] = Convert.ToInt32(values[i]);
-            query.Alternatives = choices;
-            double[] finalValues = (new BootStrap(query, fAHP)).FinalScores;
-            return string.Join(',', finalValues);
-
+                query.CreateComparisonMatrix();
+                FAHP fAHP = new FAHP(query.ComparisonMatrix);
+                double[] weights = fAHP.CriteriaWeights;
+                if (max == 23)
+                    return string.Join(',', weights);
+                int[] choices = new int[max - 23];
+                for (int i = 23; i < max; i++)
+                    choices[i - max] = Convert.ToInt32(values[i]);
+                query.Alternatives = choices;
+                double[] finalValues = (new BootStrap(query, fAHP)).FinalScores;
+                return string.Join(',', finalValues);
+            });
+            
         }
 
         /* 
