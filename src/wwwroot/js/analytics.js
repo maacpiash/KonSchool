ace20fcfad6cbcf53ecd614313cfca44d27c0e25
shunjid <shunjid1862@gmail.com>
2019-09-02 13:04:49 +0600

    added Auto Scroll, fixed design

--- src/wwwroot/js/analytics.js
+++ src/wwwroot/js/analytics.js
@@ -3,9 +3,16 @@ var konSchoolAPI = 'api';
 
 $(document).ready(function() {
     $("#mapCol a").click(function() {
+        scrollToAnalyticalResultDivision();
         requestToAPI = $.get(konSchoolAPI + '/schools/dis/' +  $(this).data("value").toUpperCase() , function (data, status) {
             console.log(data);
             console.log(status);
         });
     });
 });
+
+function scrollToAnalyticalResultDivision(){
+    $('html, body').animate({
+        scrollTop: $("#analyticalResults").offset().top
+    }, 1000);
+}
