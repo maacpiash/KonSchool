13fb4445d1a6811d855446210b7c7321aff5361d
Ahad Chowdhury <ahadc@maacpiash.com>
2019-12-04 21:00:14 +0600

    Initiate development with .NET Core 3.1 (LTS)
    This commit removes the previous code (2.1) and initiates 6 projects in
    server, client and lib directories: 3 source and 3 xUnit test projects.
    
    Each pair of projects are added inside a solution. The 3 source projects
    are also part of one solution in the root directory.

---
+++ lib/src/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace KonSchool.Lib
+{
+    public class Class1
+    {
+    }
+}
---
+++ server/test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace KonSchool.Server.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
