2c587d7a4a211f0f03484498fd2d617e33b6aa47
Ahad Chowdhury <ahad@maacpiash.com>
2020-07-14 02:50:38 +1000

    Load env values from `.env` file for dev mode
    The `.env` file should be at the root directory with the `sln` file.
    
    Also, logger is added via DI for logging info and errors.

--- src/Services/SchoolService.cs
+++ src/Services/SchoolService.cs
@@ -1,8 +1,10 @@
 using System.IO;
 using System.Collections.Generic;
-using System.Net;
-using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
+using DotNetEnv;
 using KonSchool.Models;
 using static System.Environment;
 
@@ -11,13 +13,60 @@ namespace KonSchool.Services
     public class SchoolService : ISchoolService
     {
 		readonly IMongoCollection<School> Schools;
+		private readonly ILogger _logger;
+		private string[] keys, env;
+		private int connStr = 0, dbName = 1, collName = 2;
 
-        public SchoolService()
+        public SchoolService(IWebHostEnvironment hostEnvironment, ILogger<SchoolService> logger)
         {
-			var client = new MongoClient(GetEnvironmentVariable("CONNECTIONSTRING"));
-            var database = client.GetDatabase(GetEnvironmentVariable("DBNAME"));
-            Schools = database.GetCollection<School>(GetEnvironmentVariable("COLLECTIONNAME"));
+			_logger = logger;
+			keys = new string[] { "CONNECTIONSTRING", "DBNAME", "COLLECTIONNAME" };
+			env = new string[3];
+			if (hostEnvironment.IsDevelopment())
+			{
+				var loadOptions = new Env.LoadOptions(parseVariables: true);
+				string envPath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, ".env");
+				try
+				{
+					if (File.Exists(envPath))
+						_logger.LogInformation($"Trying to read environmental variables from {envPath}");
+					else
+						throw new System.Exception($"Environmental variables file not found at {envPath}");
+					Env.Load(envPath, loadOptions);
+					for (int i = 0; i < 3; i++)
+						env[i] = Env.GetString(keys[i]);
+					foreach (var value in env)
+						if (value is null)
+							throw new System.Exception($"Value of {value} not found in file {envPath}");
+					_logger.LogInformation($"Environmental variables are read from {envPath}");
+				}
+				catch (System.Exception e)
+				{
+					_logger.LogError(e.Message);
+					TryToGetValuesFromEnv();
+				}
+			}
+			else
+				TryToGetValuesFromEnv();
+
+			var client = new MongoClient(env[connStr]);
+            var database = client.GetDatabase(env[dbName]);
+            Schools = database.GetCollection<School>(env[collName]);
         }
+
+		private void TryToGetValuesFromEnv()
+		{
+			try
+			{
+				for (int i = 0; i < 3; i++)
+					env[i] = GetEnvironmentVariable(keys[i]);
+			}
+			catch (System.Exception ex)
+			{
+				_logger.LogError("Environmental variables not found in the environment.");
+				throw ex;
+			}
+		}
 
         public IEnumerable<School> GetSchools() => Schools.Find(Schools => true).ToList();
     }
