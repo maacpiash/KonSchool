63eea50fdcdf2109733354695c4f40527c9b811e
Ahad Chowdhury <ahad@maacpiash.com>
2020-07-15 18:36:45 +1000

    Remove references to libraries that are not needed

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Http;
-
-using KonSchool.Models;
 using System.ComponentModel.DataAnnotations;
 
 namespace KonSchool.Pages
--- src/Pages/Outputs.cshtml.cs
+++ src/Pages/Outputs.cshtml.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Http;
 using KonSchool.Models;
 using KonSchool.Services;
