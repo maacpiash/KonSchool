50c0d568f17eb72370a978fd7a2ae925440d6af6
Ahad Chowdhury <ahad@maacpiash.com>
2020-07-16 19:26:17 +1000

    Add tests for IndexModel
    These tests check whether the values are set according to inputs.

--- tests/PageModelTests/IndexModel.test.cs
+++ tests/PageModelTests/IndexModel.test.cs
@@ -30,6 +30,59 @@ namespace KonSchool.Tests.PageModelTests
             Assert.Equal(NUMBER_OF_OCCUPATIONS, indexPage.Occupations.Count);
         }
 
+		[Theory]
+		[InlineData(0, "Female")]
+		[InlineData(1, "Male")]
+		public void Can_SetSex_OnPost(int input, string sex)
+		{
+			var indexPage = new IndexModel(httpCtxAccessor.Object)
+			{
+				Class = "6",
+                Division = "Dhaka",
+                District = "Faridpur",
+                Thana = "Kamarkhand",
+                UW = "Raydoulotpur [Chowbari]",
+                Occupation = "Farmer",
+				Sex = input
+			};
+			Assert.True(indexPage.ModelState.IsValid);
+			indexPage.OnPost();
+			Assert.Equal(sex, httpCtxAccessor.Object.HttpContext.Session.GetString("_Sex"));
+		}
+
+		[Theory]
+		[InlineData("Worker", 2.5)]
+		[InlineData("Tati", 2.5)]
+		[InlineData("Fisherman", 2.5)]
+		[InlineData("Kamar/Kumar", 2.5)]
+		[InlineData("Cultivation", 5.0)]
+		[InlineData("Expatriate", 5.0)]
+		[InlineData("Small business", 5.0)]
+		[InlineData("Govt. job", 7.5)]
+		[InlineData("Private job", 7.5)]
+		[InlineData("Teacher", 7.5)]
+		[InlineData("Lawyer", 10.0)]
+		[InlineData("Doctor", 10.0)]
+		[InlineData("Engineer", 10.0)]
+		[InlineData("Businessman", 10.0)]
+		[InlineData("Superman", 1.0)]
+		public void Can_SetSocial_OnPost(string input, double ses)
+		{
+			var indexPage = new IndexModel(httpCtxAccessor.Object)
+			{
+				Class = "6",
+                Division = "Dhaka",
+                District = "Faridpur",
+                Thana = "Kamarkhand",
+                UW = "Raydoulotpur [Chowbari]",
+				Occupation = input
+			};
+			Assert.True(indexPage.ModelState.IsValid);
+			indexPage.OnPost();
+			double.TryParse(httpCtxAccessor.Object.HttpContext.Session.GetString("_Social"), out double value);
+			Assert.Equal(ses, value);
+		}
+
         [Fact]
         public void Can_RedirectTo_InputsPage_OnPost()
         {
