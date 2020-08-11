363b5993f26b92db1aa93776597024e7db7ad203
Ahad Chowdhury <ahad@maacpiash.com>
2020-08-11 23:47:13 +1000

    Refactor JavaScript for updating location selects
    Optimization and modernization — just shows how bad I was in JS 2y ago!

--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -3,80 +3,32 @@
 
 // Write your Javascript code.
 
-function DivDistChange(selectObj) {
-    var idx = selectObj.selectedIndex;
-    var which = selectObj.options[idx].value;
-    cList = DivDist[which];
-    var cSelect = document.getElementById("district");
-    var len = cSelect.options.length;
-    while (cSelect.options.length > 0) {
-        cSelect.remove(0);
+function updateSelect(selectObjId, elementName, dictionary, callBack) {
+  const selectObj = document.getElementById(selectObjId)
+  const idx = selectObj.selectedIndex
+  const which = selectObj.options[idx].value
+  const cList = dictionary[which]
+  const cSelect = document.getElementById(elementName)
+  while (cSelect.options.length > 0) {
+    cSelect.remove(0)
+  }
+  let newOption
+  // create new options
+  for (let i = 0; i < cList.length; i++) {
+    newOption = document.createElement('option')
+    newOption.value = cList[i] // assumes option string and value are the same
+    newOption.text = cList[i]
+    // add the new option
+    try {
+      cSelect.add(newOption)  // this will fail in DOM browsers but is needed for IE
     }
-    var newOption;
-    // create new options
-    for (var i = 0; i < cList.length; i++) {
-        newOption = document.createElement("option");
-        newOption.value = cList[i];  // assumes option string and value are the same
-        newOption.text = cList[i];
-        // add the new option
-        //try {
-        //    cSelect.add(newOption);  // this will fail in DOM browsers but is needed for IE
-        //}
-        //catch (e) {
-        cSelect.appendChild(newOption);
-        //}
-
+    catch (e) {
+      cSelect.appendChild(newOption)
     }
-    DistThanaChange(cSelect);
+  }
+  if (callBack) return callBack(cSelect)
 }
 
-function DistThanaChange(selectObj) {
-    // get the index of the selected option
-    var idx = selectObj.selectedIndex;
-    // get the value of the selected option
-    var which = selectObj.options[idx].value;
-    // use the selected option value to retrieve the list of items from the DivDist array
-    cList = DistThana[which];
-    // get the country select element via its known id
-    var cSelect = document.getElementById("thana");
-    // remove the current options from the country select
-    var len = cSelect.options.length;
-    while (cSelect.options.length > 0) {
-        cSelect.remove(0);
-    }
-    var newOption;
-    // create new options
-    for (var i = 0; i < cList.length; i++) {
-        newOption = document.createElement("option");
-        newOption.value = cList[i];  // assumes option string and value are the same
-        newOption.text = cList[i];
-        // add the new option
-        cSelect.appendChild(newOption);
-    }
-    ThanaUWChange(cSelect);
-}
-
-function ThanaUWChange(selectObj) {
-    // get the index of the selected option
-    var idx = selectObj.selectedIndex;
-    // get the value of the selected option
-    var which = selectObj.options[idx].value;
-    // use the selected option value to retrieve the list of items from the DivDist array
-    cList = Dict[which];
-    // get the country select element via its known id
-    var cSelect = document.getElementById("uw");
-    // remove the current options from the country select
-    var len = cSelect.options.length;
-    while (cSelect.options.length > 0) {
-        cSelect.remove(0);
-    }
-    var newOption;
-    // create new options
-    for (var i = 0; i < cList.length; i++) {
-        newOption = document.createElement("option");
-        newOption.value = cList[i];  // assumes option string and value are the same
-        newOption.text = cList[i];
-        // add the new option
-        cSelect.appendChild(newOption);
-    }
-}
+const ThanaUWChange = () => updateSelect('thana', 'uw', Dict)
+const DistThanaChange = () => updateSelect('district', 'thana', DistThana, ThanaUWChange)
+const DivDistChange = () => updateSelect('division', 'district', DivDist, DistThanaChange)
