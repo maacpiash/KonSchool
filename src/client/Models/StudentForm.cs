b4a95a588dd0537bb94fee70ffbe28fb8633c2b1
Ahad Chowdhury <ahad@maacpiash.com>
2021-02-16 23:11:53 +1100

    Add student information form

---
+++ src/client/Models/StudentForm.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace KonSchool.Client.Models
+{
+	public class StudentFormModel
+	{
+		[Required] [Range(6, 10, ErrorMessage = "Class {0} must be between {1} and {2}.")]
+		public int Class { get; set; }
+
+		[Required] [Range(1, 100, ErrorMessage = "Age {0} must be between {1} and {2}.")]
+		public int Age { get; set; }
+
+		[Required]
+		public bool IsFemale { get; set; }
+
+		[Required]
+		public string Occupation { get; set; }
+
+		[Required]
+		public string Division { get; set; }
+
+		[Required]
+		public string District { get; set; }
+
+		[Required]
+		public string Thana { get; set; }
+
+		[Required]
+		public string Union_Ward { get; set; }
+
+	}
+}
