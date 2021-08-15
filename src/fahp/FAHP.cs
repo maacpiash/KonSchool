5b2fc9d2eb3ee46de99f1fb4c20a37d0bc34ed8a
Ahad Chowdhury <ahad@maacpiash.com>
2021-08-15 20:42:59 +1000

    Upgrade the FAHP service to .NET 6 preview 7
    The changes from preview 4 come mainly from non-nullable references:
    route values are checked before the `try`-`catch` block in the API
    delegate method, and all the properties of `FAHP` class are set inside
    the `ctor`.
    
    The top-level namespace declaration is used in all the files.
    
    This commit also contains minor upgrades to the `Serilog` packages.

--- src/fahp/FAHP.cs
+++ src/fahp/FAHP.cs
@@ -1,84 +1,66 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-
 using static System.Math;
 using static KonSchool.Shared.Stat;
 
-namespace KonSchool.FAHP
+namespace KonSchool.FAHP;
+
+public class FAHP
 {
-	public class FAHP
+	public readonly int CriteriaCount;
+	public double[] CriteriaWeights { get; set; }
+	public (double, double, double)[,] ComparisonMatrix;
+
+	public FAHP((double, double, double)[,] ComparisonMatrix)
 	{
-		public readonly int CriteriaCount;
+		CriteriaCount = ComparisonMatrix.GetLength(0);
+		this.ComparisonMatrix = ComparisonMatrix;
+		CriteriaWeights = IsValid() ? RunFAHP() : new double[CriteriaCount];
+	}
 
-		private double[] criteriaWeights;
-		public double[] CriteriaWeights
+	private bool IsValid()
+	{
+		for (int i = 0; i < CriteriaCount; i++)
 		{
-			get => criteriaWeights ?? (ValidMatrix ? RunFAHP() : null);
-			set => criteriaWeights = value;
+			if (!ComparisonMatrix[i, i].Equals((1.0, 1.0, 1.0)))
+				return false;
+		}
+		return true;
+	}
+
+	private double[] RunFAHP()
+	{
+		(double, double, double) temp;
+
+		// Step 1 : Geometric Mean
+		double[] weights = new double[CriteriaCount];
+		double power = 1.0 / 6.0;
+		(double, double, double)[] geomean = new (double, double, double)[CriteriaCount];
+		for (int i = 0; i < CriteriaCount; i++)
+		{
+			temp = (1.0, 1.0, 1.0);
+			for (int j = 0; j < CriteriaCount; j++)
+				temp = temp.ScalarMultiply(ComparisonMatrix[i, j]);
+			geomean[i] = (Pow(temp.Item1, power), Pow(temp.Item2, power), Pow(temp.Item3, power));
 		}
 
-		public bool ValidMatrix { get; set; }
-
-		public (double, double, double)[,] ComparisonMatrix;
-
-		public static (double, double, double)[] TFNs;
-
-		public FAHP((double, double, double)[,] ComparisonMatrix)
+		// Step 2 : Multiplying with Inverse Vector
+		double L = 0, M = 0, U = 0;
+		for (int i = 0; i < CriteriaCount; i++)
 		{
-			CriteriaCount = ComparisonMatrix.GetLength(0);
-			this.ComparisonMatrix = ComparisonMatrix;
-			ValidMatrix = IsValid();
-			if (ValidMatrix) RunFAHP();
+			L += geomean[i].Item1;
+			M += geomean[i].Item2;
+			U += geomean[i].Item3;
 		}
 
-		private bool IsValid()
+		for (int i = 0; i < CriteriaCount; i++)
 		{
-			for (int i = 0; i < CriteriaCount; i++)
-			{
-				if (!ComparisonMatrix[i, i].Equals((1.0, 1.0, 1.0)))
-					return false;
-			}
-			return true;
+			var now = geomean[i];
+			now = (now.Item1 / U, now.Item2 / M, now.Item3 / L);
+			weights[i] = (now.Item1 + now.Item2 + now.Item3) / 3;
 		}
 
-		private double[] RunFAHP()
-		{
-			(double, double, double) temp;
+		// Step 5: Normalization
+		NormalizeBySum(ref weights);
 
-			// Step 1 : Geometric Mean
-			double[] weights = new double[CriteriaCount];
-			double power = 1.0 / 6.0;
-			(double, double, double)[] geomean = new (double, double, double)[CriteriaCount];
-			for (int i = 0; i < CriteriaCount; i++)
-			{
-				temp = (1.0, 1.0, 1.0);
-				for (int j = 0; j < CriteriaCount; j++)
-					temp = temp.ScalarMultiply(ComparisonMatrix[i, j]);
-				geomean[i] = (Pow(temp.Item1, power), Pow(temp.Item2, power), Pow(temp.Item3, power));
-			}
-
-			// Step 2 : Multiplying with Inverse Vector
-			double L = 0, M = 0, U = 0;
-			for (int i = 0; i < CriteriaCount; i++)
-			{
-				L += geomean[i].Item1;
-				M += geomean[i].Item2;
-				U += geomean[i].Item3;
-			}
-
-			for (int i = 0; i < CriteriaCount; i++)
-			{
-				var now = geomean[i];
-				now = (now.Item1 / U, now.Item2 / M, now.Item3 / L);
-				weights[i] = (now.Item1 + now.Item2 + now.Item3) / 3;
-			}
-
-			// Step 5: Normalization
-			NormalizeBySum(ref weights);
-
-			return weights;
-		}
+		return weights;
 	}
 }
--- src/fahp/Program.cs
+++ src/fahp/Program.cs
@@ -1,53 +1,46 @@
-using System;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
 using Serilog;
 
-namespace KonSchool.FAHP
+namespace KonSchool.FAHP;
+
+public class Program
 {
-	public class Program
+	static async Task Main(string[] args)
 	{
-		static async Task Main(string[] args)
+		var builder = WebApplication.CreateBuilder(args);
+
+		var app = builder.Build();
+
+		if (builder.Environment.IsDevelopment())
+			app.UseDeveloperExceptionPage();
+		else
+			app.UseHsts();
+		app.UseHttpsRedirection();
+
+		app.MapGet("/{numbers}", async context =>
 		{
-			var builder = WebApplication.CreateBuilder(args);
-
-			await using var app = builder.Build();
-
-			if (app.Environment.IsDevelopment())
-				app.UseDeveloperExceptionPage();
-			else
-				app.UseHsts();
-			app.UseHttpsRedirection();
-
-			app.MapGet("/{numbers}", async context =>
+			using var log = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+			var numbers = context.Request.RouteValues["numbers"];
+			if (numbers is not null)
 			{
-				using var log = new LoggerConfiguration().WriteTo.Console().CreateLogger();
-				var numbers = (string) context.Request.RouteValues["numbers"];
-				if (numbers is not null)
+				try
 				{
-					try
-					{
-						var values = Array.ConvertAll(numbers.Split(','), int.Parse);
-						var fahp = new FAHP(Inference.ComparisonMatrix(values));
-						log.Information($"Returning values for {numbers}.");
-						var weights = fahp.CriteriaWeights;
-						var weightsStr = string.Join(", ", weights.Select(x => x.ToString("0.00000")).ToArray());
-						log.Information($"Values: {weightsStr}");
-						await context.Response.WriteAsJsonAsync(weights);
-					}
-					catch (Exception e)
-					{
-						log.Error(e.Message);
-						context.Response.StatusCode = 400;
-						await context.Response.WriteAsync($"{e.Message}: {numbers}");
-					}
+					var values = Array.ConvertAll(((string)numbers).Split(','), int.Parse);
+					var fahp = new FAHP(Inference.ComparisonMatrix(values));
+					log.Information($"Returning values for {numbers}.");
+					var weights = fahp.CriteriaWeights;
+					var weightsStr = string.Join(", ", weights.Select(x => x.ToString("0.00000")).ToArray());
+					log.Information($"Values: {weightsStr}");
+					await context.Response.WriteAsJsonAsync(weights);
 				}
-			});
+				catch (Exception e)
+				{
+					log.Error(e.Message);
+					context.Response.StatusCode = 400;
+					await context.Response.WriteAsync($"{e.Message}: {numbers}");
+				}
+			}
+		});
 
-			await app.RunAsync();
-		}
+		await app.RunAsync();
 	}
 }
