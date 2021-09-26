bf8268dee337984c6a142e18e0da15dcb7f5d178
Ahad Chowdhury <ahad@maacpiash.com>
2021-09-26 21:34:33 +1000

    Refactor `Program.cs` of both APIs
    Let's get "minimal" 🚀

--- src/fahp/Program.cs
+++ src/fahp/Program.cs
@@ -1,73 +1,65 @@
 using System;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using KonSchool.FAHP;
 
-namespace KonSchool.FAHP;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
 {
-	static async Task Main(string[] args)
+	options.SwaggerDoc("v1", new OpenApiInfo
 	{
-		var builder = WebApplication.CreateBuilder(args);
-
-		builder.Services.AddEndpointsApiExplorer();
-		builder.Services.AddSwaggerGen(options =>
+		Version = "v1",
+		Title = "KonSchool Web API for FAHP",
+		Description = "Calculates weights of criteria via Fuzzy Analytic Hierarchy Process (FAHP)",
+		Contact = new OpenApiContact
 		{
-			options.SwaggerDoc("v1", new OpenApiInfo
-			{
-				Version = "v1",
-				Title = "KonSchool Web API for FAHP",
-				Description = "Calculates weights of criteria via Fuzzy Analytic Hierarchy Process (FAHP)",
-				Contact = new OpenApiContact
-				{
-					Name = "Mohammad Abdul Ahad Chowdhury",
-					Email = "ahad@maacpiash.com",
-					Url = new Uri("https://www.maacpiash.com"),
-				},
-				License = new OpenApiLicense
-				{
-					Name = "AGPL-3.0",
-					Url = new Uri("https://github.com/maacpiash/KonSchool/blob/master/LICENSE"),
-				},
-			});
-		});
-
-		var app = builder.Build();
-
-		app.UseSwagger();
-		app.UseSwaggerUI();
-
-		if (builder.Environment.IsDevelopment())
-			app.UseDeveloperExceptionPage();
-		else
-			app.UseHsts();
-		app.UseHttpsRedirection();
-
-		app.MapGet("/{numbers}", (string numbers) =>
+			Name = "Mohammad Abdul Ahad Chowdhury",
+			Email = "ahad@maacpiash.com",
+			Url = new Uri("https://www.maacpiash.com"),
+		},
+		License = new OpenApiLicense
 		{
-			try
-			{
-				var values = Array.ConvertAll((numbers).Split(','), int.Parse);
-				var fahp = new FAHP(Inference.ComparisonMatrix(values));
-				app.Logger.LogInformation($"Returning values for {numbers}.");
-				var weights = fahp.CriteriaWeights;
-				var weightsStr = string.Join(", ", weights.Select(x => x.ToString("0.00000")).ToArray());
-				app.Logger.LogInformation($"Values: {weightsStr}");
-				return Results.Ok(weights);
-			}
-			catch (Exception e)
-			{
-				app.Logger.LogError(e.Message);
-				return Results.BadRequest($"{e.Message}: {numbers}");
-			}
-		});
+			Name = "AGPL-3.0",
+			Url = new Uri("https://github.com/maacpiash/KonSchool/blob/master/LICENSE"),
+		},
+	});
+});
 
-		await app.RunAsync();
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+if (builder.Environment.IsDevelopment())
+	app.UseDeveloperExceptionPage();
+else
+	app.UseHsts();
+app.UseHttpsRedirection();
+
+app.MapGet("/{numbers}", (string numbers) =>
+{
+	try
+	{
+		var values = Array.ConvertAll((numbers).Split(','), int.Parse);
+		var fahp = new FAHP(Inference.ComparisonMatrix(values));
+		app.Logger.LogInformation($"Returning values for {numbers}.");
+		var weights = fahp.CriteriaWeights;
+		var weightsStr = string.Join(", ", weights.Select(x => x.ToString("0.00000")).ToArray());
+		app.Logger.LogInformation($"Values: {weightsStr}");
+		return Results.Ok(weights);
 	}
-}
+	catch (Exception e)
+	{
+		app.Logger.LogError(e.Message);
+		return Results.BadRequest($"{e.Message}: {numbers}");
+	}
+});
+
+app.Run();
--- src/schools/Program.cs
+++ src/schools/Program.cs
@@ -1,88 +1,80 @@
 using System;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using static Microsoft.AspNetCore.Http.Results;
+using KonSchool.Schools;
 
-namespace KonSchool.Schools;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<ISchoolsRepository, SchoolsRepository>();
 
-public class Program
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
 {
-	static async Task Main(string[] args)
+	options.SwaggerDoc("v1", new OpenApiInfo
 	{
-		var builder = WebApplication.CreateBuilder(args);
-		builder.Services.AddSingleton<ISchoolsRepository, SchoolsRepository>();
-
-		builder.Services.AddEndpointsApiExplorer();
-		builder.Services.AddSwaggerGen(options =>
+		Version = "v1",
+		Title = "KonSchool Web API for Schools",
+		Description = "Provides informations on secondary schools in Bangladesh",
+		Contact = new OpenApiContact
 		{
-			options.SwaggerDoc("v1", new OpenApiInfo
-			{
-				Version = "v1",
-				Title = "KonSchool Web API for Schools",
-				Description = "Provides informations on secondary schools in Bangladesh",
-				Contact = new OpenApiContact
-				{
-					Name = "Mohammad Abdul Ahad Chowdhury",
-					Email = "ahad@maacpiash.com",
-					Url = new Uri("https://www.maacpiash.com"),
-				},
-				License = new OpenApiLicense
-				{
-					Name = "AGPL-3.0",
-					Url = new Uri("https://github.com/maacpiash/KonSchool/blob/master/LICENSE"),
-				}
-			});
-		});
-
-		var app = builder.Build();
-
-		app.UseSwagger();
-		app.UseSwaggerUI();
-
-		if (builder.Environment.IsDevelopment())
-			app.UseDeveloperExceptionPage();
-		else
-			app.UseHsts();
-		app.UseHttpsRedirection();
-
-		app.MapGet("/{eiin:int}", (int eiin, ISchoolsRepository schools) =>
+			Name = "Mohammad Abdul Ahad Chowdhury",
+			Email = "ahad@maacpiash.com",
+			Url = new Uri("https://www.maacpiash.com"),
+		},
+		License = new OpenApiLicense
 		{
-			var school = schools.GetAllSchools().GetSchool(eiin);
+			Name = "AGPL-3.0",
+			Url = new Uri("https://github.com/maacpiash/KonSchool/blob/master/LICENSE"),
+		}
+	});
+});
 
-			if (school is null)
-			{
-				app.Logger.LogInformation($"School with EIIN {eiin} not found.");
-				return NotFound();
-			}
+var app = builder.Build();
 
-			app.Logger.LogInformation($"School with EIIN {eiin} found.");
-			return Ok(school);
-		});
+app.UseSwagger();
+app.UseSwaggerUI();
 
-		app.MapGet("/", (string? div, string? dis, string? sex, bool? seg, int? @class, ISchoolsRepository schools) =>
-		{
-			var filteredSchools = schools
-				.GetAllSchools()
-				.FilterByDivision(div)
-				.FilterByDistrict(dis)
-				.FilterBySex(sex)
-				.FilterBySegregated((seg ?? false) ? sex : null)
-				.FilterByClass(@class);
+if (builder.Environment.IsDevelopment())
+	app.UseDeveloperExceptionPage();
+else
+	app.UseHsts();
+app.UseHttpsRedirection();
 
-			app.Logger.LogInformation($"{filteredSchools.Count()} school(s) found with the following query:\n" +
-				$"Division:\t{div}\nDistrict:\t{dis}\nSex:\t\t{sex}\nSegragated:\t{seg}\nClass:\t\t{@class}\n");
+app.MapGet("/{eiin:int}", (int eiin, ISchoolsRepository schools) =>
+{
+	var school = schools.GetAllSchools().GetSchool(eiin);
 
-			if (filteredSchools.Count() == 0)
-				return NotFound();
-
-			return Ok(filteredSchools);
-		});
-
-		await app.RunAsync();
+	if (school is null)
+	{
+		app.Logger.LogInformation($"School with EIIN {eiin} not found.");
+		return NotFound();
 	}
-}
+
+	app.Logger.LogInformation($"School with EIIN {eiin} found.");
+	return Ok(school);
+});
+
+app.MapGet("/", (string? div, string? dis, string? sex, bool? seg, int? @class, ISchoolsRepository schools) =>
+{
+	var filteredSchools = schools
+		.GetAllSchools()
+		.FilterByDivision(div)
+		.FilterByDistrict(dis)
+		.FilterBySex(sex)
+		.FilterBySegregated((seg ?? false) ? sex : null)
+		.FilterByClass(@class);
+
+	app.Logger.LogInformation($"{filteredSchools.Count()} school(s) found with the following query:\n" +
+		$"Division:\t{div}\nDistrict:\t{dis}\nSex:\t\t{sex}\nSegragated:\t{seg}\nClass:\t\t{@class}\n");
+
+	if (filteredSchools.Count() == 0)
+		return NotFound();
+
+	return Ok(filteredSchools);
+});
+
+app.Run();
