68c4e8a5c3ba417c4e5ccd9ec77ae687cbd46b1e
Ahad Chowdhury <ahad@maacpiash.com>
2021-12-15 23:10:18 +1100

    Add location and occupation data

---
+++ src/app/Data/Occupations.cs
@@ -0,0 +1,23 @@
+namespace KonSchool.App.Data;
+
+public static class Occupations
+{
+	public static System.Collections.Generic.List<string> All = new()
+	{
+		"Worker",
+		"Fisherman",
+		"Tati",
+		"Kamar/Kumar",
+		"Cultivation",
+		"Expatriate",
+		"Small business",
+		"Govt. job",
+		"Private job",
+		"Teacher",
+		"Lawyer",
+		"Doctor",
+		"Engineer",
+		"Businessman",
+		"Other"
+	};
+}
--- src/app/Program.cs
+++ src/app/Program.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using KonSchool.App;
+using KonSchool.App.Data;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
+builder.Services.AddSingleton<ILocationDataService, LocationDataService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
